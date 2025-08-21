40c1c5c312a26dc5d23f5897b3f90be748265032
Sunil Sharma <Arnil1718@gmail.com>
2025-08-21 19:10:06 +0530

    Update usettings.py

--- plugins/usettings.py
+++ plugins/usettings.py
@@ -1,81 +1,63 @@
 import time
-from pyrogram import filters, Client as mergeApp
+from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup
 from helpers.msg_utils import MakeButtons
 from helpers.utils import UserSettings
 
-
-@mergeApp.on_message(filters.command(["settings"]))
-async def f1(c: mergeApp, m: Message):
-    # setUserMergeMode(uid=m.from_user.id,mode=1)
-    replay = await m.reply(text="Please wait", quote=True)
+@Client.on_message(filters.command(["settings"]))
+async def f1(c: Client, m: Message):
+    status_msg = await m.reply("Please wait...", quote=True)
     usettings = UserSettings(m.from_user.id, m.from_user.first_name)
-    await userSettings(
-        replay, m.from_user.id, m.from_user.first_name, m.from_user.last_name, usettings
-    )
-
+    await userSettings(status_msg, m.from_user.id, m.from_user.first_name, m.from_user.last_name, usettings)
 
 async def userSettings(
     editable: Message,
     uid: int,
-    fname,
-    lname,
+    fname: str,
+    lname: str,
     usettings: UserSettings,
 ):
     b = MakeButtons()
-    if usettings.user_id:
-        if usettings.merge_mode == 1:
-            userMergeModeId = 1
-            userMergeModeStr = "Video 🎥 + Video 🎥"
-        elif usettings.merge_mode == 2:
-            userMergeModeId = 2
-            userMergeModeStr = "Video 🎥 + Audio 🎵"
-        elif usettings.merge_mode == 3:
-            userMergeModeId = 3
-            userMergeModeStr = "Video 🎥 + Subtitle 📜"
-        elif usettings.merge_mode == 4:
-            userMergeModeId = 4
-            userMergeModeStr = "Extract" 
-        if usettings.edit_metadata:
-            editMetadataStr = "✅"
-        else:
-            editMetadataStr = "❌"
-        uSettingsMessage = f"""
-<b><u>Merge Bot settings for <a href='tg://user?id={uid}'>{fname} {lname}</a></u></b>
-    ┃
-    ┣**👦 ID: <u>{usettings.user_id}</u>**
-    ┣**{'🚫' if usettings.banned else '🫡'} Ban Status: <u>{usettings.banned}</u>**
-    ┣**{'⚡' if usettings.allowed else '❗'} Allowed: <u>{usettings.allowed}</u>**
-    ┣**{'✅' if usettings.edit_metadata else '❌'} Edit Metadata: <u>{usettings.edit_metadata}</u>**
-    ┗**Ⓜ️ Merge mode: <u>{userMergeModeStr}</u>**
+    # Determine merge mode string
+    modes = {
+        1: "Video 🎥 + Video 🎥",
+        2: "Video 🎥 + Audio 🎵",
+        3: "Video 🎥 + Subtitle 📜",
+        4: "Extract Streams 🔄",
+        5: "URL Merge 🔗"
+    }
+    mode_id = usettings.merge_mode if usettings.merge_mode in modes else 1
+    mode_str = modes.get(mode_id, modes[1])
+
+    edit_meta = "✅" if usettings.edit_metadata else "❌"
+    ban_status = "🚫 BANNED" if usettings.banned else "✅ Active"
+    allow_status = "⚡ Allowed" if usettings.allowed else "❗ Not Allowed"
+
+    text = f"""
+<b><u>Merge Bot Settings</u></b>
+<b>User:</b> <a href='tg://user?id={uid}'>{fname} {lname}</a>
+<b>ID:</b> {uid}
+<b>Status:</b> {ban_status} | {allow_status}
+<b>Edit Metadata:</b> {edit_meta}
+<b>Merge Mode:</b> {mode_str}
 """
-        markup = b.makebuttons(
-            [
-                "Merge mode",
-                userMergeModeStr,
-                "Edit Metadata",
-                editMetadataStr,
-                "Close",
-            ],
-            [
-                "tryotherbutton",
-                f"ch@ng3M0de_{uid}_{(userMergeModeId%4)+1}",
-                "tryotherbutton",
-                f"toggleEdit_{uid}",
-                "close",
-            ],
-            rows=2,
-        )
-        res = await editable.edit(
-            text=uSettingsMessage, reply_markup=InlineKeyboardMarkup(markup)
-        )
-    else:
-        usettings.name = fname
-        usettings.merge_mode = 1
-        usettings.allowed = False
-        usettings.edit_metadata = False
-        usettings.thumbnail = None
-        await userSettings(editable, uid, fname, lname, usettings)
-    # await asyncio.sleep(10)
-    # await c.delete_messages(chat_id=editable.chat.id, message_ids=[res.id-1,res.id])
-    return
+
+    buttons = [
+        [
+            InlineKeyboardButton("🔀 Change Mode", callback_data=f"ch@ng3M0de_{uid}_{(mode_id % len(modes)) + 1}"),
+            InlineKeyboardButton("✏️ Toggle Metadata", callback_data=f"toggleEdit_{uid}")
+        ],
+        [
+            InlineKeyboardButton("❌ Close", callback_data="close")
+        ]
+    ]
+
+    await editable.edit(
+        text=text,
+        reply_markup=InlineKeyboardMarkup(b.makebuttons(
+            [ "Change Mode", mode_str, "Edit Metadata", edit_meta, "Close" ],
+            [ f"ch@ng3M0de_{uid}_{(mode_id % len(modes)) + 1}", "ignore",
+              f"toggleEdit_{uid}", "ignore", "close" ],
+            rows=2
+        ))
+    )
